f51c2644030c20a5842564cfdfb0175f4e243537
Bruno T Prinzo <brunoprinzo@hotmail.com>
2020-12-09 20:32:27 -0300

    Exception Handling

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,25 +5,21 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticateUser = new AuthenticateUserService();
+  const authenticateUser = new AuthenticateUserService();
 
-    const { user, token } = await authenticateUser.execute({ email, password });
+  const { user, token } = await authenticateUser.execute({ email, password });
 
-    const userWithOutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      createdAt: user.created_at,
-      updatedAt: user.updated_at,
-    };
+  const userWithOutPassword = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    createdAt: user.created_at,
+    updatedAt: user.updated_at,
+  };
 
-    return response.json({ user: userWithOutPassword, token });
-  } catch (error) {
-    return response.status(400).json({ error: error.message });
-  }
+  return response.json({ user: userWithOutPassword, token });
 });
 
 export default sessionsRouter;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,35 +1,55 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({ name, email, password });
+  const user = await createUser.execute({ name, email, password });
+
+  const userWithOutPassword = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    createdAt: user.created_at,
+    updatedAt: user.updated_at,
+  };
+
+  return response.json(userWithOutPassword);
+});
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFileName: request.file.filename,
+    });
 
     const userWithOutPassword = {
       id: user.id,
       name: user.name,
       email: user.email,
+      avatar: user.avatar,
       createdAt: user.created_at,
       updatedAt: user.updated_at,
     };
 
-    return response.json(userWithOutPassword);
-  } catch (error) {
-    return response.status(400).json({ error: error.message });
-  }
-});
-
-usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
-  return response.json({ ok: true });
-});
-
+    return response.json({ user: userWithOutPassword });
+  },
+);
 export default usersRouter;
